3d1aa77c45649f3579982c48d7fffcc20d9fa9c6
Aurelien <aurelien@MacBook-Pro-de-Apple.local>
2020-06-18 10:51:55 +0200

    avant la BDD

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,8 +12,19 @@ class MainController extends AbstractController
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
+        return $this->render('index.html.twig', [
             'controller_name' => 'home',
+            'nb_category' => 8
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login_page")
+     */
+    public function login()
+    {
+        return $this->render('login_page.html.twig', [
+            'controller_name' => 'login',
         ]);
     }
 
