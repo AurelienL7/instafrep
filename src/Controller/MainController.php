ccd17ea53bb7eba538dab964a8304faa5e4bdaf5
Aurelien <aurelien@MacBook-Pro-de-Apple.local>
2020-06-22 15:08:06 +0200

    form fixed

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,11 +5,7 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\FormTypeInterface;
 
 class MainController extends AbstractController
 {
