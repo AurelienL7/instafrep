b20c7bd515de113355ce7b034df068ed303e70ea
Aurelien <aurelien@MacBook-Pro-de-Apple.local>
2020-06-23 11:41:22 +0200

    Ajout d'une page login

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,17 +5,31 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $encoder;
+
+    /**
+     * UserFixtures constructor.
+     * @param $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
 
+        $hash = $this->encoder->encodePassword($user, "azerty");
+
         $user->setUsername('Aurélien');
         $user->setCreatedAt(new \DateTime());
         $user->setAnniversary(new \DateTime("1993-06-07"));
-        $user->setPassword("azerty");
+        $user->setPassword($hash);
         $user->setCity("Bordeaux");
 
 
